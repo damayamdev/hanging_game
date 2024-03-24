96fd26728db6a1f8fff7150b6d5b4bfcc76325eb
damayamdev <164294520+damayamdev@users.noreply.github.com>
2024-03-24 14:07:22 -0500

    HangmanWord

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,10 +1,24 @@
-
 const HangmanWord = () => {
+  const word = "test";
+  const guessedLetters = ["t"]
   return (
-    <div>
-      
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span style={{ visibility: guessedLetters.includes(letter) ? "visible" : "hidden" }}>{letter}</span>
+        </span>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default HangmanWord
+export default HangmanWord;
